5abc342b01d3decc6bf630c33ea4475bb8c1d6b7
Syed Asad Ali <135553555+Asad-Ali-vfx@users.noreply.github.com>
2026-01-02 16:27:00 +0500

    Simplify age variable declaration

--- Lab 1/Task 1.cpp
+++ Lab 1/Task 1.cpp
@@ -8,8 +8,7 @@
 using namespace std;
 
 int main() {
-	int age;
-	age = 10;
+	int age= 10
 	cout << "You are " << age << " years old" << endl;
 	cout << "You are too young to play the game";
 	return 0;
